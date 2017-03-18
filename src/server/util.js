b3f83e826b14c830db7eda6ec6b5e48328671913
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2017-03-18 20:26:10 +0200

    Fix error in html template literal

--- src/server/util.js
+++ src/server/util.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const html = (strings, ...keys) => {
   strings.forEach(item => item.replace(/\n\s*/g, ''));
-  String.raw(strings, ...keys);
+  return String.raw(strings, ...keys);
 };
 /* eslint-enable import/prefer-default-export */
